96611eb9dbc5ded80ca0db4aa2844e498a0c3d6b
Ajroud-Yassine <80514203+Ajroud-Yassine@users.noreply.github.com>
2022-01-14 09:58:06 +0100

    Ajout Cercle Polygon et segment

--- TD2/Exo1/script.js
+++ TD2/Exo1/script.js
@@ -11,22 +11,58 @@ var options = {
 function success(pos) {
 
     var map = L.map('map').setView([51, -0.09], 5);
+    
+    
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+        attribution: '&copy; <a href="https://stamen-tiles-{S}.a.ssl.fastly.net">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     var crd = pos.coords;
     
     var long = crd.longitude;
     var lat = crd.latitude;
-    console.log(long)
-    console.log(lat)
     map.setView([lat,long ], 10);
-
+ 
     L.marker([lat, long]).addTo(map)
     .bindPopup('IUT')
     .openPopup();
+
+    L.marker([43.70313, 7.26608]).addTo(map)
+    .bindPopup('Nice')
+    .openPopup();
+
+    L.marker([43.296482, 5.36978]).addTo(map)
+    .bindPopup('Marseille')
+    .openPopup();
+
+    // Cercle
+    var circle = L.circle([lat, long], {
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.3,
+        radius: 1500
+    }).addTo(map);
+
+    //Triangle
+
+    var polygon = L.polygon([
+        [lat, long],
+        [43.70313, 7.26608],
+        [43.296482, 5.36978],
+        
+    ],{
+        color: 'red',
+        fillOpacity: 0.2
+    }).addTo(map);
+
+    var segment = L.polygon([
+        [43.70313, 7.26608],
+        [43.296482, 5.36978],
+        
+    ],{
+        color: 'yellow'
+    }).addTo(map);
 
 
   }
@@ -37,7 +73,6 @@ function success(pos) {
   
   navigator.geolocation.getCurrentPosition(success, error, options);
 
-// LafletJs
 
 
 
