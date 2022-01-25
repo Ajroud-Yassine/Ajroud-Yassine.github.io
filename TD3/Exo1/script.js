7fb45495fa678248ee894b7240c29a30d3c19e3d
Ajroud-Yassine <80514203+Ajroud-Yassine@users.noreply.github.com>
2022-01-25 11:06:11 +0100

    push TD3 exo1

--- TD3/Exo1/script.js
+++ TD3/Exo1/script.js
@@ -6,7 +6,26 @@ var canvas = document.getElementById("myCanvas");
     ctx.drawImage(img, 0, 0);
 
     // Maison
-    ctx.fillSTyle ='green';
+    ctx.fillStyle ="blue";
     ctx.fillRect(300,1000,250,200);
+
+    // Porte
+    ctx.fillStyle ="green";
+    ctx.fillRect(380,1050,100,150);
+    
+    // Soleil
+    ctx.beginPath();
+    ctx.arc(150,150,100,0,Math.PI*2,true);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+}
+
+
+function ciel()
+{
+
+    let ciel = document.getElementById('ciel');
+    ciel.style.fill = "rgb(6, 35, 54)"
 
 }
